af3ce77b340b3b0ad10da1706f519b852d8d144c
Lavender <anyangolavenderr@gmail.com>
2024-09-11 16:11:20 +0300

    Updating models to use AbstractUser

--- themis/user/models.py
+++ themis/user/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
 # Define choices for the role field
@@ -9,38 +8,11 @@ ROLE_CHOICES = [
     ('judge', 'Judge'),
 ]
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_staff', True)  # Ensure is_staff is set to True
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(email, password, **extra_fields)
-
-class User(AbstractBaseUser, PermissionsMixin):
-    user_ID = models.AutoField(primary_key=True)
-    email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+class User(AbstractUser):
     is_active = models.BooleanField(_('active'), default=True)
-    is_staff = models.BooleanField(_('staff status'), default=False)  # Add this line
     role = models.CharField(_('role'), max_length=10, choices=ROLE_CHOICES)
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
     updated_at = models.DateTimeField(_('updated at'), auto_now=True)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'role']
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
