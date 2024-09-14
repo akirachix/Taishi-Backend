94707142e14decebb2cda887b459fc6b88e03886
Nancy-Nabacwa <nancypaulinenabacwa@gmail.com>
2024-09-14 09:41:48 +0300

    made changes to authentication

--- themis/api/urls.py
+++ themis/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import SignupView, LoginView, UserDetailView, UserListView, ProfileListView
+from .views import SignupView, LoginView, UserDetailView, UserListView, ProfileListView,ProfileView
+from . import views
+
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='user_signup'),
@@ -7,7 +9,7 @@ urlpatterns = [
     path('users/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
     path('users/', UserListView.as_view(), name='user_list'),
     path('profiles/', ProfileListView.as_view(), name='profile_list'),
-    
-
+    path('profiles/<int:pk>/', ProfileView.as_view(), name='profile_list'),
+    path('generate_token/', views.generate_token, name='generate_token'),
 
 ]
--- themis/authentication/views.py
+++ themis/authentication/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from urllib.parse import quote_plus, urlencode
 from django.contrib.auth import login
+from django.views.decorators.csrf import csrf_exempt
 
 oauth = OAuth()
+
 
 oauth.register(
     "auth0",
@@ -17,7 +19,7 @@ oauth.register(
     },
     server_metadata_url=f"http://{settings.AUTH0_DOMAIN}/.well-known/openid-configuration",
 )
-
+@csrf_exempt
 def login(request):
     return oauth.auth0.authorize_redirect(
         request, request.build_absolute_uri(reverse("callback"))
