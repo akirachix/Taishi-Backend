4bf277df2e4bf88111faf5552061a208ed52779e
Glory_Kuria <kuriaglory@gmail.com>
2024-09-16 12:57:51 +0300

    added api and authentication

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from .views import SignupView, LoginView, UserDetailView, UserListView, ProfileListView,ProfileView
+from . import views
+urlpatterns = [
+    path('signup/', SignupView.as_view(), name='user_signup'),
+    path('login/', LoginView.as_view(), name='user_login'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('users/', UserListView.as_view(), name='user_list'),
+    path('profiles/', ProfileListView.as_view(), name='profile_list'),
+    path('profiles/<int:pk>/', ProfileView.as_view(), name='profile_list'),
+    path('generate_token/', views.generate_token, name='generate_token'),
+]
