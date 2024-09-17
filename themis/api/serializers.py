ad991fe2184496f1520c9aa77d01cf6831b980e6
Glory_Kuria <kuriaglory@gmail.com>
2024-09-17 10:01:16 +0300

    Added CORS

---
+++ themis/api/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from user.models import User
+from user_profile.models import UserProfile
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "role",
+            "is_active",
+            "created_at",
+            "updated_at",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+    def create(self, validated_data):
+        if User.objects.filter(email=validated_data["email"]).exists():
+            raise serializers.ValidationError(
+                {"email": "This email is already in use."}
+        )
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            password=validated_data["password"],
+            first_name=validated_data["first_name"],  # Corrected
+            last_name=validated_data["last_name"],    # Corrected
+            role=validated_data.get("role", "user"),
+        )
+        return user
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = UserProfile
+        fields = ["user", "profile_picture"]
